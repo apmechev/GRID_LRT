17ecd03b5f975e31fa31bd2187b318e0ebb256ee
Alexandar Mechev <apmechev@loui.grid.sara.nl>
2016-04-04 12:11:06 +0200

    define resuberr to false

--- fad-master.py
+++ fad-master.py
@@ -6,7 +6,8 @@
 #		Added check for proper input and srm filename
 #		Added check for staging and if staging succeeded
 #version 1.1- 17-Mar-2016
-#
+#version 1.2 - 3-Apr-2015
+#		Now accepts a default parset with no master_setup
 ##########################
 
 import glob
@@ -27,16 +28,22 @@ if len(sys.argv)<3:
 	print "ex.  python fad.py srm_L229587.txt master_setup.cfg"
 	sys.exit()
 
-if ("srm" in sys.argv[-2]) and ("master_setup.cfg" in sys.argv[-1]):
+if ("srm" in sys.argv[-2]) and (("master_setup.cfg" in sys.argv[-1]) or ("parset" in sys.argv[-1])):
 	srmfile=sys.argv[-2]
-	mastercfg=sys.argv[-1]
-elif ("srm" in sys.argv[-1]) and ("master_setup.cfg" in sys.argv[-2]):
+	if ("parset" in sys.argv[-1]):
+		parsetfile=sys.argv[-1]
+	else:
+		mastercfg=sys.argv[-1]
+elif ("srm" in sys.argv[-1]) and (("master_setup.cfg" in sys.argv[-2]) or ("parset" in sys.argv[-2])):
         srmfile=sys.argv[-1]
-        mastercfg=sys.argv[-2]
+        if ("parset" in sys.argv[-2]):
+                parsetfile=sys.argv[-2]
+	else:
+		mastercfg=sys.argv[-2]
 else: 
 	print "there may be a typo in your filenames"
 	sys.exit()
-
+resuberr=False
 if ("-r" in sys.argv[:-2] or ("--resub-error-only" in sys.argv[:-2])):
 	print "\033[226mFlag set to resubmit only error tokens\033[0m"
 	resuberr=True
@@ -93,6 +100,14 @@ shutil.copy("srmlist",fadir+"/Tokens/datasets/"+obsid)
 shutil.copy("subbandlist",fadir+"/Tokens/datasets/"+obsid)
 shutil.copy("srmlist",fadir+"/Staging/datasets/"+obsid)
 shutil.copy("subbandlist",fadir+"/Staging/datasets/"+obsid)
+try:
+	parsetfile
+except NameError:
+	pass
+else:
+	shutil.copy(fadir+"parsets"+parsetfile,fadir+"/Tokens/datasets/"+obsid)
+	shutil.copy(fadir+"parsets"+parsetfile,fadir+"/Staging/datasets/"+obsid)
+
 
 
 ################
