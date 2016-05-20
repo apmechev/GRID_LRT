efa9c56118364a3074135023dd4bffde8b3fd185
Alexandar Mechev <apmechev@loui.grid.sara.nl>
2016-05-20 09:46:01 +0200

    Adding Prefactor in the sandbox

---
+++ FAD_v1/Application/sandbox/prefactor/allimports.py
@@ -0,0 +1,176 @@
+import matplotlib as mpl
+import matplotlib
+import lofar.parmdb
+import sys
+import os
+import scipy
+import time
+import numpy
+import math
+import pyrap.tables
+import scipy.signal
+import scipy.ndimage
+import scipy.interpolate
+#import lofar.expion.fitting as fitting
+#import splprep, splev
+#import Rbf, InterpolatedUnivariateSpline
+import sys, os, glob, re
+import numpy as np
+import shutil
+#import progressbar
+import logging
+import pyrap.tables as pt
+import lofar.parmdb
+import losoto._version
+import losoto._logging
+import h5parm, solWriter, solFetcher
+import multiprocessing as mp
+import matplotlib.pyplot as plt
+import numpy as np
+import factorial
+import matplotlib as mpl
+import matplotlib
+import lofar.parmdb
+import sys
+import os
+import scipy
+import time
+import numpy
+import math
+import pyrap.tables
+import scipy.signal
+import scipy.ndimage
+import scipy.interpolate
+import lofar.expion.fitting as fitting
+import splprep, splev
+import Rbf, InterpolatedUnivariateSpline
+import sys, os, glob, re
+import numpy as np
+import shutil
+import progressbar
+import logging
+import pyrap.tables as pt
+import lofar.parmdb
+import losoto._version
+import losoto._logging
+import h5parm, solWriter, solFetcher
+import multiprocessing as mp
+import matplotlib.pyplot as plt
+import numpy as np
+import factorial
+import numpy
+import pyrap.tables as pt
+import sys
+import argparse
+import RawTextHelpFormatter
+import pyrap.images as pim
+import pyrap.tables as pt
+import numpy as np
+import matplotlib as mpl
+import numpy as np
+import os,sys
+import pylab
+import matplotlib as mpl
+import numpy as np
+import scipy.signal as s
+import sys
+import os
+import h5parm, solWriter, solFetcher
+import pylab as pl
+import matplotlib as mpl
+import matplotlib
+import lofar.parmdb
+import sys
+import os
+import scipy
+import time
+import numpy
+import math
+import pyrap.tables
+import pp
+import scipy.signal
+import sys, os, glob, re
+import numpy as np
+import shutil
+import progressbar
+import logging
+import pyrap.tables as pt
+import lofar.parmdb
+import losoto._version
+import losoto._logging
+import h5parm, solWriter, solFetcher
+import multiprocessing as mp
+import lofar.expion.fitting as fitting
+import measures
+import casacore.tables as pt
+import casacore.quanta as qa
+import numpy as np
+import os, sys
+import pyrap.tables as pt
+import sys, os
+import numpy as np
+import DataMap
+import DataProduct
+import numpy
+import os, sys
+import DataMap
+import DataProduct
+import numpy as np
+import pyrap.tables as pt
+import lofar.parmdb as pdb
+import h5parm, solWriter
+import logging
+import optparse
+import glob
+import argparse
+import RawTextHelpFormatter
+import bdsm
+import bdsm
+import pyrap.images as pim
+import pickle
+import numpy as np
+import sys
+import os
+import argparse
+import RawTextHelpFormatter
+import os
+import lsmtool
+import sys
+import argparse
+import RawTextHelpFormatter
+import os
+import numpy as np
+import lofar.parmdb as pdb
+import lofar.parmdb as lp
+import numpy as np
+import sys, os
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import argparse
+import argparse
+import RawTextHelpFormatter
+import pyrap.images as pim
+import pyrap.tables as pt
+import numpy as np
+import matplotlib as mpl
+import aplpy
+import numpy
+import astropy.io.fits
+import pyrap.tables as pt
+import sys, os
+import numpy as np
+import DataMap
+import DataProduct
+import optparse
+import glob
+import random
+import os
+import argparse
+import pickle
+import copy
+import os
+import numpy as np
+import sys
+import glob
+import pyrap.tables as pt
+import lofar.parmdb as pdb
