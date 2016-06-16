e63bb24ad83369030fcb67301bdf6ddd64fb2fd1
Alexandar Mechev <apmechev@loui.grid.sara.nl>
2016-06-16 13:44:52 +0200

    Each download process now reports its interface sent/received bytes on eth*
    Former-commit-id: e69d709ef13325d2b4c65a941b63e66794dd4045

--- FAD_v1/Application/sandbox/prefactor/bin/download_num_files.sh
+++ FAD_v1/Application/sandbox/prefactor/bin/download_num_files.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 echo "Downloading Subbands "${1}" to "${2}" from file "${3}
-for i in $(seq ${1} ${2}); do head -n $i ${3}|tail -n 1|  awk '{ print $1}' |xargs prefactor/bin/getfiles.sh & done
-
+for i in $(seq ${1} ${2}); do head -n $i ${3}|tail -n 1|  awk '{ print $1}' |xargs prefactor/bin/getfiles.sh &  echo $!" SB"$i>>activejobs; done #get line, get first column, execute as argument to getfiles.sh
