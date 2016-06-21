4d8af49a52abf715aa8bcc66a1d4c1bb550ea99a
Alexandar Mechev <apmechev@loui.grid.sara.nl>
2016-06-21 12:15:55 +0200

    Added functions to fad.py
    Former-commit-id: 06c7ed28d5faf643372a9582abaec65f4ffe8719

--- fad.py
+++ fad.py
@@ -26,71 +26,222 @@ import subprocess
 # Some checks on input arguments
 ############
 
-if len(sys.argv)<3:
-	print ""
-	print "You need to input the SRM file and the master config file"
-	print "ex.  python fad-master.py srm_L229587.txt master_setup.cfg"
-	print "optional flags ( -r or --resub-error-only) come after fad-master.py "
-	print "optional turn off Time Splitting (-noTS or --no-time-splitting) as well"
-	sys.exit()
 
-if ("srm" in sys.argv[-2]) and (".cfg" in sys.argv[-1]):
-	srmfile=sys.argv[-2]
-	mastercfg=sys.argv[-1]
+def parse_arguments(args):
+	if len(args)<3:
+		print ""
+		print "You need to input the SRM file and the master config file"
+		print "ex.  python fad-master.py srm_L229587.txt master_setup.cfg"
+		print "optional flags ( -r or --resub-error-only) come after fad-master.py "
+		print "optional turn off Time Splitting (-noTS or --no-time-splitting) as well"
+		sys.exit()
+	
+	if ("srm" in args[-2]) and (".cfg" in args[-1]):
+		srmfile=sys.argv[-2]
+		mastercfg=sys.argv[-1]
+	
+	elif ("srm" in args[-1]) and (".cfg" in args[-2]):
+	        srmfile=args[-1]
+		mastercfg=args[-2]
+	
+	else: 
+		print "there may be a typo in your filenames"
+		sys.exit()
+	resuberr=False
+	TSplit=True
 
-elif ("srm" in sys.argv[-1]) and (".cfg" in sys.argv[-2]):
-        srmfile=sys.argv[-1]
-	mastercfg=sys.argv[-2]
+	if ("-r" in args[:-2] or ("--resub-error-only" in args[:-2])):
+		print "\033[33mFlag set to resubmit only error tokens\033[0m"
+		resuberr=True
+	
+	if ("-noTS" in args[:-2] or ("--no-time-splitting" in args[:-2])):
+	        print "\033[33mTurning Off Timesplitting\033[0m"
+	        TSplit=False
 
-else: 
-	print "there may be a typo in your filenames"
-	sys.exit()
-resuberr=False
-TSplit=True
-if ("-r" in sys.argv[:-2] or ("--resub-error-only" in sys.argv[:-2])):
-	print "\033[33mFlag set to resubmit only error tokens\033[0m"
-	resuberr=True
+	if srmfile== 'srm.txt': #If filename is just srm.txt TODO: Maybe catch other filenames
+		with open(srmfile,'r') as f:
+			line=f.readline()
+			obsid='L'+str(re.search("L(.+?)_",line).group(1))
+			print "copying srm.txt into srm_L"+obs_name+".txt "
+		shutil.copyfile('srm.txt','srm_'+obs_name+'.txt')
+		srmfile='srm_'+obsid+'.txt'
+	parsetfile=""
+	with open(mastercfg,'r') as readparset:
+		for line in readparset:
+			if "PARSET" in line:
+				parsetfile=line.split("PARSET",1)[1].split("= ")[1].split('\n')[0]
 
-if ("-noTS" in sys.argv[:-2] or ("--no-time-splitting" in sys.argv[:-2])):
-        print "\033[33mTurning Off Timesplitting\033[0m"
-        TSplit=False
-
-if srmfile== 'srm.txt': #If filename is just srm.txt TODO: Maybe catch other filenames
-	with open(srmfile,'r') as f:
-		line=f.readline()
-		obs_name='L'+str(re.search("L(.+?)_",line).group(1))
-		print "copying srm.txt into srm_L"+obs_name+".txt "
-	shutil.copyfile('srm.txt','srm_'+obs_name+'.txt')
-	srmfile='srm_'+obs_name+'.txt'
-parsetfile=""
-with open(mastercfg,'r') as readparset:
-	for line in readparset:
-		if "PARSET" in line:
-			parsetfile=line.split("PARSET",1)[1].split("= ")[1].split('\n')[0]
-
-
-
+	with open(srmfile, 'r') as f:
+	         obsid=re.search('L[0-9]*',f.readline()).group(0)
+	
+	#check if obsid exists in srm file\033[0m
+	found=False
+	with open(srmfile,'rt') as f:
+	        for line in f:
+	                if obsid in line:
+	                        found=True
+	                        print("Processing OBSID=\033[32m"+obsid+"\033[0m")
+	        if not found:
+	                print "\033[31mOBSID not found in SRM file!\033[0m"
+	                sys.exit()
+	
+	return(resuberr,TSplit,parsetfile,obsid,srmfile)	
+	
+	
 ###########
 #re-extracts the FAD tarfile if needed and sets up fadir
 ###########
-print ""
-print "You're running the \033[33m FAD1.0 \033[0m Time-Splitting is \033[33m"+["OFF","ON"][TSplit]+"\033[0m"+[" By User Request"," By Default"][TSplit]+"!"
-print ""
-
-latest_tar=glob.glob('FAD_*[0-9]*.tar')[-1]
-if not latest_tar:
-	fadir=glob.glob('FAD_*[0-9]*')[-1]
-if not os.path.isdir(os.path.splitext(latest_tar)[0]):
-	print "Extracting "+ latest_tar
-	tar=tarfile.open(latest_tar)
-	tar.extractall()
-	tar.close()
-
-if latest_tar:
-	fadir=os.path.splitext(latest_tar)[0]
+def setup_dirs(TSplit):
+	print ""
+	print "You're running \033[33m FAD1.0\033[0m Time-Splitting is \033[33m"+["OFF","ON"][TSplit]+"\033[0m"+[" By User Request"," By Default"][TSplit]+"!"
+	print ""
 	
-sys.path.append(str(fadir+'/gsurl'))
-import gsurl_v3
+	latest_tar=glob.glob('FAD_*[0-9]*.tar')[-1]
+	if not latest_tar:
+		fadir=glob.glob('FAD_*[0-9]*')[-1]
+	if not os.path.isdir(os.path.splitext(latest_tar)[0]):
+		print "Extracting "+ latest_tar
+		tar=tarfile.open(latest_tar)
+		tar.extractall()
+		tar.close()
+	
+	if latest_tar:
+		fadir=os.path.splitext(latest_tar)[0]
+		
+	sys.path.append(str(fadir+'/gsurl'))
+	import gsurl_v3
+	return fadir
+
+
+####################
+#PICAS Database Submission
+#####################
+def submit_to_picas(resuberr):
+        try:
+            print "Your picas user is "+os.environ["PICAS_USR"]+" and the DB is "+os.environ["PICAS_DB"]
+        except KeyError:
+            print "\033[31m You haven't set $PICAS_USR or $PICAS_DB or $PICAS_USR_PWD! \n\n Exiting\033[0m"
+            sys.exit()
+
+        if resuberr:
+                subprocess.call(['python','resetErrorTokens.py',os.environ["PICAS_DB"],os.environ["PICAS_USR"],os.environ["PICAS_USR_PWD"]])
+        else:
+                subprocess.call(['python','removeObsIDTokens.py',obsid,os.environ["PICAS_DB"],os.environ["PICAS_USR"],os.environ["PICAS_USR_PWD"]])
+                subprocess.call(['python','createTokens.py',obsid,os.environ["PICAS_DB"],os.environ["PICAS_USR"],os.environ["PICAS_USR_PWD"]])
+        subprocess.call(['python','createViews.py',os.environ["PICAS_DB"],os.environ["PICAS_USR"],os.environ["PICAS_USR_PWD"]])
+        subprocess.call(['python','createObsIDView.py',obsid,os.environ["PICAS_DB"],os.environ["PICAS_USR"],os.environ["PICAS_USR_PWD"]])
+        os.chdir("../../")
+
+        os.remove('srmlist')
+        os.remove('subbandlist')
+
+####################
+#Submit job using glite-wms-job-submit
+#As it turns out, in avg_dmx.jdl, the PICAS variables are not evaluated (new environment)
+#to make this truly portable, (and safe), they are evaluated and replaced in the script
+#and after the file is restored 
+#####################
+
+def start_jdl(jdl_file):
+        if os.path.exists(fadir+"/Application/jobIDs"):
+                os.remove(fadir+"/Application/jobIDs")
+
+        os.chdir(fadir+"/Application")
+        subprocess.call(["ls","-lat","sandbox/scripts/parsets"])
+        #TODO: Change avg_dmx's number of jobs to number of subbands
+
+        dmx_jdl=['avg_dmx_no-TS.jdl','avg_dmx.jdl'][TSplit] #If Tsplit=True (Default), file is avg_dmx.jdl else the other one
+        shutil.copy(dmx_jdl,'avg_dmx_with_variables.jdl')
+        filedata=None
+        with open(dmx_jdl,'r') as file:
+            filedata = file.read()
+        filedata = filedata.replace('$PICAS_DB $PICAS_USR $PICAS_USR_PWD', os.environ["PICAS_DB"]+" "+os.environ["PICAS_USR"]+" "+os.environ["PICAS_USR_PWD"])
+
+
+        os.remove(dmx_jdl)
+        numprocess=sum(1 for line in open("../../"+srmfile,'rt'))
+        filedata=filedata.replace("Parameters=50","Parameters="+str(numprocess))
+        with open(dmx_jdl,'w+') as file:
+            file.write(filedata)
+
+
+        subprocess.call(['glite-wms-job-submit','-d',os.environ["USER"],'-o','jobIDs',dmx_jdl])
+
+        shutil.move('avg_dmx_with_variables.jdl',dmx_jdl)
+
+##############
+#Prepares the sandbox and zips and uploads to storage
+#This will be pulled by the job on the node
+#while a variable with the path will be passed to the token
+##############
+def prepare_sandbox(fadir,obsid,customscript=""):
+	os.chdir(fadir+"/Application/sandbox")
+	try:
+	        os.remove("scripts.tar")
+	except OSError:
+	        pass
+	if customscript!="":
+		shutil.copy("../../../../"+customscript, "scripts/avg_dmx2.py")	
+	print("tarring everything")
+	subprocess.call(["tar","-cf", "scripts.tar","scripts/"])
+	
+
+
+	
+	os.chdir("../")
+	subprocess.call(["tar","-cf", "sandbox.tar","sandbox/"])
+	sandbox_base_dir="gsiftp://gridftp.grid.sara.nl:2811/pnfs/grid.sara.nl/data/lofar/user/disk/spectroscopy/sandbox"
+        print "uploading sandbox to storage for pull by nodes"
+
+	subprocess.call(["uberftp", "-rm",sandbox_base_dir+"/sandbox_"+os.environ["PICAS_USR"]+".tar"])
+	subprocess.call(['globus-url-copy', "file:"+os.environ["PWD"]+"/"+fadir+"/Application/sandbox.tar",sandbox_base_dir+"/sandbox_"+os.environ["PICAS_USR"]+".tar"])	
+
+        os.chdir("../../")
+	return
+
+
+if __name__ == "__main__":
+        [resuberr,Tsplit,parsetfile,obsid,srm_file]=parse_arguments(sys.argv)
+	fadir=setup_dirs(Tsplit)
+        prepare_sandbox(fadir,obsid)
+	
+	####################
+	##Wait for keystroke
+	###################
+
+	yes = set(['yes','y', 'ye','yea','yesh','Y','oui', ''])
+	no = set(['no','n','N'])
+	print ""
+        print "Do you want to continue? Y/N (Enter continues)"
+
+	from termios import tcflush, TCIOFLUSH
+	tcflush(sys.stdin, TCIOFLUSH) #Flush input buffer to stop enter-spammers
+	choice = raw_input().lower()
+
+	if choice in yes:
+	   pass
+	elif choice in no:
+	   sys.exit()
+	else:
+	   sys.exit()
+
+	submit_to_picas(resuberr)
+	sys.exit()
+
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#WALL OF OLD CODE BELOW
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+
+
+
 
 ###########
 #Clean Directories and old parset
@@ -113,16 +264,6 @@ with open(srmfile, 'r') as f:
 	 obsid=re.search('L[0-9]*',f.readline()).group(0)
 
 #check if obsid exists in srm file
-found=False
-with open(srmfile,'rt') as f:
-	for line in f:
-		if obsid in line:
-			found=True
-			print obsid
-	if not found:
-		print "\033[31mOBSID not found in SRM file!\033[0m"
-		sys.exit()
-	
 
 
 os.makedirs(fadir+'/Tokens/datasets/'+obsid)
@@ -140,15 +281,6 @@ if not ((len(parsetfile)<4) or ("fault" in parsetfile) or parsetfile=="DEFAULT")
 	
 
 #Add tarring of parset files which will be untarred on the node
-
-#subprocess.call(['cp','-r',fadir+"/parsets/*",fadir+"/Application/sandbox/scripts/parsets"])
-os.chdir(fadir+"/Application/sandbox")
-try:
-	os.remove("scripts.tar")
-except OSError:
-	pass
-subprocess.call(["tar","-cvf", "scripts.tar","scripts/"])
-os.chdir("../../../")
 
 devnl=open(os.devnull, 'w')
 greproc=subprocess.Popen(['glite-wms-job-status','-i',"FAD_v1/Application/jobIDs"],stdout=subprocess.PIPE,stderr=devnl)
@@ -251,74 +383,4 @@ elif fileloc=='j':
 print ""
 os.chdir("../../")
 os.chdir(fadir+"/Tokens/")
-
-
-####################
-#PICAS Database Submission
-#####################
-try:
-    print "Your picas user is "+os.environ["PICAS_USR"]+" and the DB is "+os.environ["PICAS_DB"]
-except KeyError:
-    print "\033[31m You haven't set $PICAS_USR or $PICAS_DB or $PICAS_USR_PWD! \n\n Exiting\033[0m"
-    sys.exit()
-
-if resuberr:
-	subprocess.call(['python','resetErrorTokens.py',os.environ["PICAS_DB"],os.environ["PICAS_USR"],os.environ["PICAS_USR_PWD"]])     
-else:
-	subprocess.call(['python','removeObsIDTokens.py',obsid,os.environ["PICAS_DB"],os.environ["PICAS_USR"],os.environ["PICAS_USR_PWD"]])
-	subprocess.call(['python','createTokens.py',obsid,os.environ["PICAS_DB"],os.environ["PICAS_USR"],os.environ["PICAS_USR_PWD"]])
-subprocess.call(['python','createViews.py',os.environ["PICAS_DB"],os.environ["PICAS_USR"],os.environ["PICAS_USR_PWD"]])
-subprocess.call(['python','createObsIDView.py',obsid,os.environ["PICAS_DB"],os.environ["PICAS_USR"],os.environ["PICAS_USR_PWD"]])
-os.chdir("../../")
-
-os.remove('srmlist')
-os.remove('subbandlist')
-
-
-####################
-##Wait for keystroke
-###################
-yes = set(['yes','y', 'ye', ''])
-no = set(['no','n'])
-print "Do you want to continue? Y/N (Enter continues)"
-choice = raw_input().lower()
-
-if choice in yes:
-   pass
-elif choice in no:
-   sys.exit()
-else:
-   sys.exit()
-
-####################
-#Submit job using glite-wms-job-submit
-#As it turns out, in avg_dmx.jdl, the PICAS variables are not evaluated (new environment)
-#to make this truly portable, (and safe), they are evaluated and replaced in the script
-#and after the file is restored 
-#####################
-if os.path.exists(fadir+"/Application/jobIDs"):
-	os.remove(fadir+"/Application/jobIDs")
-
-os.chdir(fadir+"/Application")
-subprocess.call(["ls","-lat","sandbox/scripts/parsets"])
-#TODO: Change avg_dmx's number of jobs to number of subbands
-
-dmx_jdl=['avg_dmx_no-TS.jdl','avg_dmx.jdl'][TSplit] #If Tsplit=True (Default), file is avg_dmx.jdl else the other one
-shutil.copy(dmx_jdl,'avg_dmx_with_variables.jdl')
-filedata=None
-with open(dmx_jdl,'r') as file:
-    filedata = file.read()
-filedata = filedata.replace('$PICAS_DB $PICAS_USR $PICAS_USR_PWD', os.environ["PICAS_DB"]+" "+os.environ["PICAS_USR"]+" "+os.environ["PICAS_USR_PWD"])
-
-
-os.remove(dmx_jdl)
-numprocess=sum(1 for line in open("../../"+srmfile,'rt'))
-filedata=filedata.replace("Parameters=50","Parameters="+str(numprocess))
-with open(dmx_jdl,'w+') as file:
-    file.write(filedata)
-
-
-subprocess.call(['glite-wms-job-submit','-d',os.environ["USER"],'-o','jobIDs',dmx_jdl])
-
-shutil.move('avg_dmx_with_variables.jdl',dmx_jdl)
 
