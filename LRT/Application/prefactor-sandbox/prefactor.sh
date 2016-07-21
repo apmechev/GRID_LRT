230e58f039e6384f8a2c9d350c8429bc321709d1
Alexandar Mechev <apmechev@loui.grid.sara.nl>
2016-07-21 19:56:37 +0200

    Statistics.xml files uploaded to grid storage

--- LRT/Application/prefactor-sandbox/prefactor.sh
+++ LRT/Application/prefactor-sandbox/prefactor.sh
@@ -302,7 +302,7 @@ kill $TCOLL_PID
 find . -name "*png"|xargs tar -zcf pngs.tar.gz
 find . -name "*npy"|xargs tar -cf numpys.tar
 find . -name "*tcollector.out"|xargs tar -cf profile.tar
-find . -iname "statistics.xml" -exec tar -rvf profile.tar {} \;
+find . -iname "*statistics.xml" -exec tar -rvf profile.tar {} \;
 find . -iname "*h5" -exec tar -rvf numpys.tar {} \;
 
 
@@ -313,7 +313,7 @@ find . -name "*npy"
 # - step3 finished check contents
 more output
 OBSID=$( echo $(head -1 srm.txt) |grep -Po "L[0-9]*" | head -1 )
-globus-url-copy file: profile.tar gsiftp://gridftp.grid.sara.nl:2811/pnfs/grid.sara.nl/data/lofar/user/disk/profiling/profile_$OBSID.tar
+globus-url-copy file:`pwd`/profile.tar gsiftp://gridftp.grid.sara.nl:2811/pnfs/grid.sara.nl/data/lofar/user/disk/profiling/profile_$OBSID.tar
 if [[ $( grep "finished unsuccesfully" output) > "" ]]
 then
      echo "Pipeline did not finish, tarring work and run directories for re-run"
