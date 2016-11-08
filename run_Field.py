205d764f787305e53221c6cf1f8ae107abf18cd4
Alexandar Mechev <apmechev@loui.grid.sara.nl>
2016-11-08 11:43:12 +0100

    If calibrator solutions exists, run_field skips to next step

--- run_Field.py
+++ run_Field.py
@@ -1,8 +1,8 @@
 import fields_LRT
-import sys,math
+import sys,math,subprocess
 
 
-def initialize_from_file(OBSIDS_file,line,time_avg=8,freq_avg=2):
+def initialize_from_file(OBSIDS_file,line,time_avg=4,freq_avg=4):
     '''Initializes the field object from a file
         by default it sets parameters to 4chan/SB and 4sec resolution
     '''
@@ -11,11 +11,11 @@ def initialize_from_file(OBSIDS_file,line,time_avg=8,freq_avg=2):
         for linex in params:
             lines.append(linex.split(','))
     field_values=lines[int(line)]
-    targ_OBSID=field_values[0]
-    targ_freq=int(field_values[2])/freq_avg
-    targ_time=time_avg/int(math.floor(float(field_values[3])))
-    cal_freq=int(field_values[7])/freq_avg
-    cal_time=time_avg/int(math.floor(float(field_values[8])))
+    targ_OBSID=field_values[1]
+    targ_freq=int(field_values[3])/freq_avg
+    targ_time=time_avg/int(math.floor(float(field_values[4])))
+    cal_freq=int(field_values[8])/freq_avg
+    cal_time=time_avg/int(math.floor(float(field_values[9])))
     f1=fields_LRT.Field("field_"+str(targ_OBSID))
     f1.initialize("L"+str(targ_OBSID),(cal_time,targ_time),(cal_freq,targ_freq))
     print "Initialized the Field Object:"
@@ -50,12 +50,18 @@ def run_field(f_obj,cal_thresh=0.05):
 
     s1.start(f_obj.srms['targ'][0],threshold=1.) #just stages target and doesn't wait
     s2.start(f_obj.srms['cal'][0],threshold=cal_thresh) #stages and waits for the calibrator 
-    #p1.start(f_obj.srms['cal'],f_obj.parsets['cal'],f_obj.OBSIDs['cal'],f_obj.name,args=['-n','244','-d','/cvmfs/softdrive.nl/apmechev/lofar_prof','-v','2_18','-j','remote-prefactor-cal.jdl'])   
-    p1.progress=1
+    
+    calfile=subprocesss.Popen(['uberftp','-ls','gsiftp://gridftp.grid.sara.nl:2811/pnfs/grid.sara.nl/data/lofar/user/sksp/spectroscopy-migrated/prefactor/numpy_'+f_obj.OBSIDs['cal']+".tar"],stdout=subprocess.PIPE)
+    cal_results=calfile.communicate()[0] #Find whether the calibrator has been run before 
+
+    if cal_results=="":
+        p1.start(f_obj.srms['cal'],f_obj.parsets['cal'],f_obj.OBSIDs['cal'],f_obj.name,args=['-n','244','-d','/cvmfs/softdrive.nl/apmechev/lofar_prof','-v','2_18','-j','remote-prefactor-cal.jdl'])
+    else:
+        p1.progress=1
+
     s3.start(f_obj.srms['targ'][0],threshold=cal_thresh)#really waits for the target to be staged fully
     s3.start_time=s1.start_time #staging started with s1, gives realistic staging length 
     p2.start(f_obj.srms['targ'],f_obj.parsets['targ'],f_obj.OBSIDs['targ'],f_obj.name,args=['-n','1','-d','/cvmfs/softdrive.nl/apmechev/lofar_prof','-v','2_18'],prev_step=p1,calobsid=f_obj.OBSIDs['cal'])
-    #Add targ2 to parsets!!!!! 
     p3.start(f_obj.srms['targ'],f_obj.parsets['targ2'],f_obj.OBSIDs['targ'],f_obj.name,args=['-n','10','-d','/cvmfs/softdrive.nl/apmechev/lofar_prof','-v','2_18','-j','remote-prefactor-targ2.jdl'],prev_step=p2,calobsid=f_obj.OBSIDs['cal'])
 
 
