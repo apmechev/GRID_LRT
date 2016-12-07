412f71aaa148148b700dfe263cdddcf02afbdc74
Alexandar Mechev <apmechev@loui.grid.sara.nl>
2016-12-07 16:31:44 +0100

    added check if gridutils exist
    Former-commit-id: a2d6fe81b51371771e494459090030befbb3ac39

--- LRT/Application/prefactor-sandbox/bin/setup_downloads.sh
+++ LRT/Application/prefactor-sandbox/bin/setup_downloads.sh
@@ -4,7 +4,13 @@
 #This function takes the Pipeline type as $1 and executes the appropriate dl setup sub_function 
 function setup_downloads(){
 
-echo "Setting up downloads"
+echo "setup_dl: Setting up downloads"
+globus-url-copy >/dev/null 2>&1
+if [[ $? ==  127 ]]
+ then 
+    echo "setup_dl: globus-url-copy doesn't exist. ";exit 13
+fi
+
 
 if [ ! -z $( echo $1 | grep init_sub ) ]
  then
@@ -16,19 +22,19 @@ if [ ! -z $( echo $1 | grep targ2 ) ]
  setup_targ2   
 fi
 
-echo "Final srms to download"
-cat srm-final.txt
+echo "setup_dl: inal srms to download"
+cat srm.txt
 }
 
 
 function setup_init_sub(){
 
   echo ""
-  echo "processing INITIAL-SUBTRACT Parset ${PARSET}"
+  echo "setup_dl: processing INITIAL-SUBTRACT Parset ${PARSET}"
   echo ""
-  echo "Setting download of subbands in OBSID ${OBSID}"
+  echo "setup_dl: Setting download of subbands in OBSID ${OBSID}"
   uberftp -ls -r gsiftp://gridftp.grid.sara.nl:2811/pnfs/grid.sara.nl/data/lofar/user/sksp/spectroscopy-migrated/prefactor/ |grep $OBSID |awk '{print "srm://srm.grid.sara.nl:8443"$NF}' > gsiftps_init.txt
-  echo "found these gsiftps associated with ${OBSID}"
+  echo "setup_dl: found these gsiftps associated with ${OBSID}"
   cat gsiftps_init.txt
   echo ""
   rm -rf srm*txt
@@ -39,11 +45,11 @@ function setup_init_sub(){
 function setup_targ2(){
 
   echo ""
-  echo "Doing GSMCal for ${OBSID}"
+  echo "setup_dl: Doing GSMCal for ${OBSID}"
   echo ""
-  echo "Setting download of subbands in OBSID ${OBSID}"
+  echo "setup_dl: Setting download of subbands in OBSID ${OBSID}"
   uberftp -ls -r gsiftp://gridftp.grid.sara.nl:2811/pnfs/grid.sara.nl/data/lofar/user/sksp/spectroscopy-migrated/prefactor/SKSP/${OBSID}/t1* |grep $OBSID |awk '{print "srm://srm.grid.sara.nl:8443"$NF}' > gsiftps_init.txt
-  echo "found these gsiftps associated with ${OBSID}"
+  echo "setup_dl: found these gsiftps associated with ${OBSID}"
   cat gsiftps_init.txt
   echo ""
   #rm -rf srm*txt
