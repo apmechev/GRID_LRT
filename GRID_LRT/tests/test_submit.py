b1b53fd6133731b1a443dee81807a2cf05a95c34
apmechev <apmechev@gmail.com>
2017-11-29 12:38:24 +0100

    Added documentation for GRID_LRT.Application.submit, as well as unittests for it

---
+++ GRID_LRT/tests/test_submit.py
@@ -0,0 +1,40 @@
+from GRID_LRT.Application import submit
+import os 
+import glob
+import unittest
+
+
+
+class JdlsubmitTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_default_args(self):
+        launcher=submit.jdl_launcher()
+        self.assertTrue(launcher.numjobs==1)
+        self.assertTrue(launcher.token_type=='t_test')
+        self.assertTrue(launcher.NCPU==1)
+
+    def test_Numjobs(self):
+        launcher=submit.jdl_launcher(numjobs=10)
+        self.assertTrue(launcher.numjobs==10)
+        jdl_file=launcher.build_jdl_file()
+        self.assertTrue("Parameters= 10" in jdl_file)
+
+    def test_Numjobs(self):
+        launcher=submit.jdl_launcher(NCPU=10)
+        self.assertTrue(launcher.NCPU==10)
+        jdl_file=launcher.build_jdl_file()
+        self.assertTrue("CPUNumber = 10"in jdl_file)
+
+    def test_LaunchFileExists(self):
+        launcher=submit.jdl_launcher(NCPU=10)
+        jdl_file=launcher.build_jdl_file()
+        self.assertTrue(os.path.exists(launcher.launch_file))
+        self.assertTrue(os.path.isfile(launcher.launch_file))
+
+if __name__ == '__main__':
+    unittest.main() 
