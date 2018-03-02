6e943d02bca65a03a06198b1c6a7c828c72ae7d5
Alexandar Mechev <apmechev@gmail.com>
2018-03-02 13:16:52 +0100

    Adding one more stage_all_LTA test

--- GRID_LRT/tests/test_staging_LTA.py
+++ GRID_LRT/tests/test_staging_LTA.py
@@ -17,3 +17,7 @@ class Staging_Test(unittest.TestCase):
         f=os.path.dirname(__file__)+'/srm_50_sara.txt'
         stage_all_LTA.main(f)
 
+    def test_return_srmlist(self):
+        f=os.path.dirname(__file__)+'/srm_50_sara.txt'
+        l=stage_all_LTA.return_srmlist(f)
+        self.assertTrue(len(l)==51)
