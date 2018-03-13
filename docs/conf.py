3572121841be0883f6bdd3da3c230bf594c70a81
Alexandar Mechev <apmechev@gmail.com>
2018-03-13 14:36:42 +0100

    Updated Token documentation, size of sidebar for readthedocs and a few more docs things

--- docs/conf.py
+++ docs/conf.py
@@ -91,7 +91,7 @@ todo_include_todos = True
 html_theme = "classic"
 html_theme_options = {
     "rightsidebar": "false",
-    "sidebarwidth": 340,
+    "sidebarwidth": 300,
     "relbarbgcolor": "black"
 }
 
