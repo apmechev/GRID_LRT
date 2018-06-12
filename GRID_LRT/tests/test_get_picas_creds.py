59be8afde1e700191ca8f9625cfa3c05f08adc7a
apmechev <apmechev@gmail.com>
2018-06-12 12:33:29 +0200

    Just reverting to standard print

--- GRID_LRT/tests/test_get_picas_creds.py
+++ GRID_LRT/tests/test_get_picas_creds.py
@@ -2,12 +2,7 @@ from GRID_LRT import get_picas_credentials as get_pc
 import os 
 import glob
 import unittest
-
-import unittest.mock
-try:
-    import StringIO
-except ImportError:
-    import io.StringIO as StringIO
+ 
 
 
 
@@ -17,20 +12,21 @@ class picas_cred_test(unittest.TestCase):
 
     def tearDown(self):
         pass
-    @unittest.mock.patch('sys.stdout', new_callable=StringIO)
+
+
     def test_info_logging(self):
         get_pc.infolog('infolog')
-        self.assertEqual(mock_stdout.getvalue(), 'infolog')
 
-    @unittest.mock.patch('sys.stdout', new_callable=StringIO)
+
+
     def test_debug_logging(self):
         get_pc.debuglog('debuglog')
-        self.assertEqual(mock_stdout.getvalue(), 'debuglog')
 
-    @unittest.mock.patch('sys.stdout', new_callable=StringIO)
+
+
     def test_warn_logging(self):
         get_pc.warnlog('warnlog')
-        self.assertEqual(mock_stdout.getvalue(), 'warnlog')
+
         
 
 if __name__ == '__main__':
