8c6f196e2580eb805e509441a29fee0bf2f4a05b
apmechev <apmechev@gmail.com>
2018-06-16 09:53:06 +0200

    cleaning up stage_all_LTA.py

--- GRID_LRT/Staging/stage_all_LTA.py
+++ GRID_LRT/Staging/stage_all_LTA.py
@@ -16,7 +16,11 @@
 import time
 import re
 import sys
-strip = lambda x:x.strip()
+
+
+def strip(x): return x.strip()
+
+
 from GRID_LRT.Staging import stager_access as sa
 import pdb
 
@@ -27,99 +31,101 @@ def process_surl_line(line):
     """
 
     if " " in line:
-        line=line.split(" ")[0]
-    if line =="/n":
+        line = line.split(" ")[0]
+    if line == "/n":
         return
     return line
 
 
-def main(filename,test=False):
-    file_loc=location(filename)
-    rs,m=replace(file_loc)
-    with open(filename,'r') as f:
-        urls=f.readlines()
-    return (process(urls,rs,m,test))
+def main(filename, test=False):
+    file_loc = location(filename)
+    rs, m = replace(file_loc)
+    with open(filename, 'r') as f:
+        urls = f.readlines()
+    return (process(urls, rs, m, test))
+
 
 def return_srmlist(filename):
-    file_loc=location(filename)
-    rs,m=replace(file_loc)
-    f=open(filename,'r')
-    urls=f.readlines()
+    file_loc = location(filename)
+    rs, m = replace(file_loc)
+    f = open(filename, 'r')
+    urls = f.readlines()
     f.close()
-    surls=[]
+    surls = []
     for u in urls:
-        u=process_surl_line(u)
+        u = process_surl_line(u)
         if "managerv2?SFN" in u:
-            surls.append(m.sub(rs,strip(u)))
+            surls.append(m.sub(rs, strip(u)))
         else:
             surls.append(u)
     return surls
 
+
 def state_dict(srm_dict):
-    locs_options=['s','j','p']
+    locs_options = ['s', 'j', 'p']
 
-    line=srm_dict.itervalues().next()
-    file_loc=[locs_options[i] for i in range(len(locs_options)) if ["sara" in line,"juelich" in line, not "sara" in line and not "juelich" in line][i] ==True][0]
-    rs,m=replace(file_loc)
+    line = srm_dict.itervalues().next()
+    file_loc = [locs_options[i] for i in range(len(locs_options)) if [
+        "sara" in line, "juelich" in line, not "sara" in line and not "juelich" in line][i] == True][0]
+    rs, m = replace(file_loc)
 
-    urls=[]
+    urls = []
     for key, value in srm_dict.iteritems():
         urls.append(value)
-    return (process(urls,rs,m))
-
-
+    return (process(urls, rs, m))
 
 
 def location(filename):
-    locs_options=['s','j','p']
-    with open(filename,'r') as f:
-        line=f.readline()
+    locs_options = ['s', 'j', 'p']
+    with open(filename, 'r') as f:
+        line = f.readline()
 
-        file_loc=[locs_options[i] for i in range(len(locs_options)) if ["sara" in line,"juelich" in line, not "sara" in line and not "juelich" in line][i] ==True]
+        file_loc = [locs_options[i] for i in range(len(locs_options)) if [
+            "sara" in line, "juelich" in line, not "sara" in line and not "juelich" in line][i] == True]
     return file_loc[0]
 
+
 def replace(file_loc):
-    if file_loc=='p':
-        m=re.compile('/lofar')
-        repl_string="srm://lta-head.lofar.psnc.pl:8443/srm/managerv2?SFN=/lofar"
+    if file_loc == 'p':
+        m = re.compile('/lofar')
+        repl_string = "srm://lta-head.lofar.psnc.pl:8443/srm/managerv2?SFN=/lofar"
         print("Staging in Poznan")
     else:
-        m=re.compile('/pnfs')
-        if file_loc=='j':
-            repl_string="srm://lofar-srm.fz-juelich.de:8443/srm/managerv2?SFN=/pnfs/"
+        m = re.compile('/pnfs')
+        if file_loc == 'j':
+            repl_string = "srm://lofar-srm.fz-juelich.de:8443/srm/managerv2?SFN=/pnfs/"
             print("Staging in Juleich")
-        elif file_loc=='s':
-            repl_string="srm://srm.grid.sara.nl:8443/srm/managerv2?SFN=/pnfs"
+        elif file_loc == 's':
+            repl_string = "srm://srm.grid.sara.nl:8443/srm/managerv2?SFN=/pnfs"
             print("files are on SARA")
         else:
             sys.exit()
-    return repl_string,m
+    return repl_string, m
 
 
-
-def process(urls,repl_string,m,test=False):
-    surls=[]
+def process(urls, repl_string, m, test=False):
+    surls = []
     for u in urls:
         if not 'srm' in u:
-            surls.append(m.sub(repl_string,strip(u)))
+            surls.append(m.sub(repl_string, strip(u)))
         else:
             surls.append(strip(u))
-    req={}
+    req = {}
     print("Setting up "+str(len(surls))+" srms to stage")
     if test:
         return
-    stageID=sa.stage(surls)
+    stageID = sa.stage(surls)
 
     print("staged with stageID ", stageID)
     return stageID
+
 
 def get_stage_status(stageID):
     return sa.get_status(int(stageID))
 
 
-if __name__=='__main__':
-    if len(sys.argv)==2:
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
         sys.exit(main(sys.argv[1]))
     else:
         sys.exit(main('files'))
-
