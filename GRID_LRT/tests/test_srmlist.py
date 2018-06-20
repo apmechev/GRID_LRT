dd1fcd93d7a60ccb8bcacab5dd12ac566f6198c2
apmechev <apmechev@gmail.com>
2018-06-20 10:25:55 +0200

    test srmlist complies with srmlist internal members

--- GRID_LRT/tests/test_srmlist.py
+++ GRID_LRT/tests/test_srmlist.py
@@ -15,25 +15,25 @@ class SrmlistTest(unittest.TestCase):
 
     def test_append_juelich1(self):
         sl=srmlist()
-        self.assertTrue(sl.LTA_location==None)
+        self.assertTrue(sl.lta_location==None)
         sl.append("srm://lofar-srm.fz-juelich.de:8443/pnfs/fz-juelich.de/data/lofar/ops/projects/lc7_012/583127/L583127_SB010_uv.MS_c0a9adfa.tar")
-        self.assertTrue(sl.LTA_location=='juelich')
+        self.assertTrue(sl.lta_location=='juelich')
         self.assertTrue(sl[0]==sl[-1])
         self.assertTrue(sl[0]=="srm://lofar-srm.fz-juelich.de:8443/pnfs/fz-juelich.de/data/lofar/ops/projects/lc7_012/583127/L583127_SB010_uv.MS_c0a9adfa.tar")
 
     def test_append_poznan1(self):
         sl=srmlist()
-        self.assertTrue(sl.LTA_location==None)
+        self.assertTrue(sl.lta_location==None)
         sl.append("srm://lta-head.lofar.psnc.pl:8443/lofar/ops/projects/lc6_004/555005/L555005_SB302_uv.MS_805a38ae.tar")
-        self.assertTrue(sl.LTA_location=='poznan')
+        self.assertTrue(sl.lta_location=='poznan')
         self.assertTrue(sl[0]==sl[-1])
         self.assertTrue(sl[0]=="srm://lta-head.lofar.psnc.pl:8443/lofar/ops/projects/lc6_004/555005/L555005_SB302_uv.MS_805a38ae.tar")
 
     def test_append_sara1(self):
         sl=srmlist()
-        self.assertTrue(sl.LTA_location==None)
+        self.assertTrue(sl.lta_location==None)
         sl.append("srm://srm.grid.sara.nl:8443/pnfs/grid.sara.nl/data/lofar/ops/projects/lc2_038/233700/L233700_SB290_uv.dppp.MS_83fc1cc3.tar")
-        self.assertTrue(sl.LTA_location=='sara')
+        self.assertTrue(sl.lta_location=='sara')
         self.assertTrue(sl[0]==sl[-1])
         self.assertTrue(sl[0]=="srm://srm.grid.sara.nl:8443/pnfs/grid.sara.nl/data/lofar/ops/projects/lc2_038/233700/L233700_SB290_uv.dppp.MS_83fc1cc3.tar")
 
@@ -73,13 +73,13 @@ class SrmlistTest(unittest.TestCase):
 
     def test_readOBSID(self):
         sl=srmlist()
-        self.assertTrue(sl.OBSID==None)
+        self.assertTrue(sl.obsid==None)
         sl.append("srm://lofar-srm.fz-juelich.de:8443/pnfs/fz-juelich.de/data/lofar/ops/projects/lc7_012/583127/L583127_SB000_uv.MS_c0a9adfa.tar")
-        self.assertTrue(sl.OBSID=='L583127')
+        self.assertTrue(sl.obsid=='L583127')
 
     def test_two_OBSIDs(self):
         sl=srmlist()
-        self.assertTrue(sl.OBSID==None)
+        self.assertTrue(sl.obsid==None)
         sl.append("srm://lofar-srm.fz-juelich.de:8443/pnfs/fz-juelich.de/data/lofar/ops/projects/lc7_012/583127/L583127_SB000_uv.MS_c0a9adfa.tar")
         sl.append("srm://lofar-srm.fz-juelich.de:8443/pnfs/fz-juelich.de/data/lofar/ops/projects/lc7_012/583127/L583127_SB100_uv.MS_c0a9adfa.tar")
         self.assertRaises(AttributeError, sl.append, "srm://lofar-srm.fz-juelich.de:8443/pnfs/fz-juelich.de/data/lofar/ops/projects/lc7_012/583127/L581127_SB000_uv.MS_c0a9adfa.tar")
