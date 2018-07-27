27160f9d07f8110139ed8990d81d014d7b951636
Alexandar Mechev <apmechev@gmail.com>
2018-07-27 14:48:58 +0200

    fixing python3 error?

--- GRID_LRT/__init__.py
+++ GRID_LRT/__init__.py
@@ -10,16 +10,15 @@ __license__ = "GPL 3.0"
 __maintainer__ = "Alexandar P. Mechev"
 __email__ = "LOFAR@apmechev.com"
 __status__ = "Production"
-__date__ = "``2018-07-27"
+__date__ = "2018-07-27"
 
 
 
 def get_git_hash():
     proc = Popen(["git", "describe"], stdout=PIPE)
     label = proc.communicate()[0].strip()
-    g_hash=label.split(__version__+'-')[1]
-    import GRID_LRT
-    githashfile = GRID_LRT.__file__.split('__init__')[0]+"__githash__"
+    g_hash = label.split(__version__)[1][1:]
+    githashfile = __file__.split('__init__')[0]+"__githash__"
     if os.path.exists(githashfile):
        with open(githashfile) as _file:
            file_hash = _file.read()
