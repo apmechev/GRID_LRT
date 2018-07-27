9bfbaae70d04f3819ad00e7eaca2abb51d479833
Alexandar Mechev <apmechev@gmail.com>
2018-07-27 14:29:31 +0200

    Attempting to add GRID_LRT.__commit__ to __init__.py

--- GRID_LRT/__init__.py
+++ GRID_LRT/__init__.py
@@ -1,4 +1,6 @@
 """GRID_LRT: Grid LOFAR Tools"""
+import subprocess
+import os
 __all__ = ["Application", "Staging", 'sandbox', 'Token', 'couchdb', "couchdb.tests"]
 __version__ = "0.4.0"
 __author__ = "Alexandar P. Mechev"
@@ -8,4 +10,21 @@ __license__ = "GPL 3.0"
 __maintainer__ = "Alexandar P. Mechev"
 __email__ = "LOFAR@apmechev.com"
 __status__ = "Production"
+__date__ = "``2018-07-27"
 
+
+
+def get_git_hash():
+    label = subprocess.check_output(["git", "describe"]).strip()
+    g_hash=label.split(__version__+'-')[1]
+    import GRID_LRT
+    githashfile = GRID_LRT.__file__.split('__init__')[0]+"__githash__"
+    with open(githashfile) as _file:
+        file_hash = _file.read()
+    if not g_hash in file_hash:
+        with open(githashfile,'w') as _file:
+            _file.write(g_hash)
+    return g_hash
+
+
+__commit__ = get_git_hash()
