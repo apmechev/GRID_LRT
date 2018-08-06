108861447214c0219aa014fa84c8920f944b869f
Alexandar Mechev <apmechev@gmail.com>
2018-08-06 14:05:46 +0200

    fixing __init__ pylint

--- GRID_LRT/__init__.py
+++ GRID_LRT/__init__.py
@@ -1,5 +1,5 @@
 """GRID_LRT: Grid LOFAR Tools"""
-from subprocess import PIPE,Popen
+from subprocess import PIPE, Popen
 import os
 __all__ = ["Application", "Staging", 'sandbox', 'Token', 'couchdb', "couchdb.tests"]
 __version__ = "0.4.0"
@@ -15,18 +15,19 @@ __date__ = "2018-07-27"
 
 
 def get_git_hash():
+    """Gets the git hash using git describe"""
     proc = Popen(["git", "describe"], stdout=PIPE)
     label = proc.communicate()[0].strip()
-    g_hash = label.split(__version__.encode('utf-8'))[1][1:].decode('utf-8')
+    g_hash = label
     githashfile = __file__.split('__init__')[0]+"__githash__"
     if os.path.exists(githashfile):
-       with open(githashfile) as _file:
-           file_hash = _file.read().encode('utf-8')
+        with open(githashfile) as _file:
+            file_hash = _file.read().encode('utf-8')
     else:
-        file_hash=""
+        file_hash = ""
     if not g_hash in file_hash:
-        with open(githashfile,'w') as _file:
-            _file.write(g_hash)
+        with open(githashfile, 'w') as _file:
+            _file.write(str(g_hash))
     return g_hash
 
 
