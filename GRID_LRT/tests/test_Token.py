fa70367e8f6b7dd3f6ca947facc36a93d869a9d9
apmechev <apmechev@gmail.com>
2018-08-08 12:00:44 +0200

    brack at wrong place

--- GRID_LRT/tests/test_Token.py
+++ GRID_LRT/tests/test_Token.py
@@ -97,7 +97,7 @@ class TokenTest(unittest.TestCase):
         th.create_token(keys={'test_suite':'test_delete_tokens','done':0,'lock':0}, append="Tokentest", attach=[])
         th.add_status_views()
         tok = list(th.list_tokens_from_view('todo'))[0]['id']
-        th.add_attachment(tok,open(os.path.dirname(__file__)+'/srm_50_sara.txt','r','test'))
+        th.add_attachment(tok,open(os.path.dirname(__file__)+'/srm_50_sara.txt','r'),'test')
         attaches = th.list_attachments(tok)
         self.assertTrue(len(attaches)==1)
         self.assertTrue(attaches[0]=='test')
