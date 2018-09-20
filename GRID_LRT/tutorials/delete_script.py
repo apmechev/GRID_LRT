a87bf322b060800e37c481c27ee747641438e69d
Alexandar Mechev <apmechev@gmail.com>
2018-09-20 11:51:07 +0200

    adding example delete old data script

---
+++ GRID_LRT/tutorials/delete_script.py
@@ -0,0 +1,16 @@
+import datetime
+
+from GRID_LRT.Storage.utils  import GSIFile
+base_dir = 'gsiftp://gridftp.grid.sara.nl:2811/pnfs/grid.sara.nl/data/lofar/user/sksp/pipelines/SKSP/pref_targ1/'
+b_dir = GSIFile(base_dir)
+files = b_dir.list_dir()
+
+to_delete = []
+
+for f in files:
+    if f.datetime < datetime.datetime.now() - datetime.timedelta(days=31):
+        to_delete.append(f)
+
+for fi in to_delete:
+    print fi.datetime
+
