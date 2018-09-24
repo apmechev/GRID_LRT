bd64ed6684a5a087182d1ba646048585eb7bf867
Alexandar Mechev <apmechev@gmail.com>
2018-09-24 11:25:59 +0200

    adding empty storage test (only testing import for now)

---
+++ GRID_LRT/tests/test_Storage.py
@@ -0,0 +1,26 @@
+"""Testing Storage functions; For now just test imports. 
+Need to scaffold test suite using mocked uberftp -ls results"""
+
+from  GRID_LRT.Storage.utils import GSIFile 
+import unittest
+
+class GSIFileTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_creating_GSIFile(self):
+        ''' Tests creating folders on the FS in the appropriate locations
+        '''
+        pass
+
+    def test_autobuild(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+
