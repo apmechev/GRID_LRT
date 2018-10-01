8e6908b9ebf930915f46858a9e7e775131f77230
Alexandar Mechev <apmechev@gmail.com>
2018-10-01 09:24:48 +0200

    now haveGRID_LRT.__commit__ that gives the commit hash and number of commits after the release

--- GRID_LRT/__init__.py
+++ GRID_LRT/__init__.py
@@ -1,5 +1,5 @@
 """GRID_LRT: Grid LOFAR Tools"""
-from subprocess import PIPE, Popen
+from subprocess import call, check_output, STDOUT
 import os
 import socket
 
@@ -22,31 +22,33 @@ __email__ = "LOFAR@apmechev.com"
 __status__ = "Production"
 __date__ = "2018-09-29"
 
+                
 
+def format_version(version):
+    fmt = '{tag}.{commitcount}+{gitsha}'
+    parts = version.split('-')
+    assert len(parts) in (3, 4)
+    dirty = len(parts) == 4
+    tag, count, sha = parts[:3]
+    if count == '0' and not dirty:
+        return tag
+    return fmt.format(tag=tag, commitcount=count, gitsha=sha.lstrip('g'))
+
+
+def get_git_version():
+    command = 'git describe --tags --long --dirty'
+    git_version = check_output(command.split()).decode('utf-8').strip()
+    return format_version(version=git_version)
 
 def get_git_hash():
-    """Gets the git hash using git describe"""
-    g_hash = ""
-    proc = Popen(["git", "describe"], stdout=PIPE, stderr=PIPE)
-    label = proc.communicate()[0].strip().decode("utf-8") 
-    if label:
-        g_hash = label[0]
-        githashfile = __file__.split('__init__')[0]+"__githash__"
-        if os.path.exists(githashfile):
-            with open(githashfile) as _file:
-                file_hash = _file.read()
-        else:
-            file_hash = ""
-        if __version__ in g_hash:
-            g_hash = g_hash.split(__version__)[1]
-        if g_hash not in file_hash:
-            with open(githashfile, 'w') as _file:
-                _file.write(str(g_hash))
-    return g_hash
-
+    git_hash = get_git_version()
+    git_hash = git_hash.split(__version__+".")[1]
+    return {'hash':git_hash.split('+')[1],
+            'number of commits after '+__version__:git_hash.split('+')[0]}
 
 #if socket.gethostname() != 'loui.grid.surfsara.nl':
 #    RuntimeWarning("You're not running on loui. Some features will not work!")
 
-__commit__ = get_git_hash()
+if not call(["git", "branch"], stderr=STDOUT, stdout=open(os.devnull, 'w')) != 0:
+    __commit__ = get_git_hash()
 
