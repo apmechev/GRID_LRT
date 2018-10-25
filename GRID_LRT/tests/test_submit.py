1827d237ae56d46be874d7928859ee26e4cd0240
Alexandar Mechev <apmechev@gmail.com>
2018-10-25 12:43:08 +0200

    testing mocking import again

--- GRID_LRT/tests/test_submit.py
+++ GRID_LRT/tests/test_submit.py
@@ -5,9 +5,12 @@ import unittest
 import mock
 import subprocess
 
-@mock.patch.object(subprocess, 'Popen', autospec=True)
-def improt_mocked_launcher(*args, **kwargs):
-    launcher = submit.JdlLauncher(*args, **kwargs)
+
+def mocked_import(*args, **kwargs):
+    with mock.patch.object(subprocess, 'Popen') as mocked_popen:
+        mock_popen.return_value.returncode = 0
+        mock_popen.return_value.communicate.return_value = ("output", "Error")
+        launcher = submit.JdlLauncher(*args, **kwargs)
     return launcher
 
 class JdlsubmitTest(unittest.TestCase):
@@ -17,41 +20,32 @@ class JdlsubmitTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_make_tempfile(self, mock_popen):
-        mock_popen.return_value.returncode = 0
-        mock_popen.return_value.communicate.return_value = ("output", "Error")
-        launcher = improt_mocked_launcher() 
+
+    def test_make_tempfile(self):
+        launcher = mocked_import() 
         file_path = launcher.make_temp_jdlfile()
         self.assertTrue(os.path.exists(file_path.name))
-    
+
     def test_launch_raises_error(self, mock_popen):
-        mock_popen.return_value.returncode = 0
-        mock_popen.return_value.communicate.return_value = ("output", "Error")
-        launcher = improt_mocked_launcher()
+        launcher = mocked_import()
         l = launcher.make_temp_jdlfile()
         with launcher:
             self.assertRaises(OSError, launcher.launch) #Will raise OSerror if glite-wms-job-submit doesn't exist
 
     def test_default_args(self, mock_popen):
-        mock_popen.return_value.returncode = 0
-        mock_popen.return_value.communicate.return_value = ("output", "Error")
-        launcher = improt_mocked_launcher()
+        launcher = mocked_import()
         self.assertTrue(launcher.numjobs==1)
         self.assertTrue(launcher.token_type=='t_test')
         self.assertTrue(launcher.ncpu==1)
 
     def test_Numjobs(self, mock_popen):
-        mock_popen.return_value.returncode = 0
-        mock_popen.return_value.communicate.return_value = ("output", "Error")
-        launcher = improt_mocked_launcher(NCPU=10)
+        launcher = mocked_import(NCPU=10)
         self.assertTrue(launcher.ncpu==10)
         jdl_file = launcher.build_jdl_file()
         self.assertTrue("CPUNumber = 10"in jdl_file)
 
     def test_LaunchFileExists(self, mock_popen):
-        mock_popen.return_value.returncode = 0
-        mock_popen.return_value.communicate.return_value = ("output", "Error")
-        launcher = improt_mocked_launcher(NCPU=10)
+        launcher = mocked_import()NCPU=10)
         jdl_file = launcher.build_jdl_file()
         self.assertTrue(os.path.exists(launcher.launch_file))
         self.assertTrue(os.path.isfile(launcher.launch_file))
