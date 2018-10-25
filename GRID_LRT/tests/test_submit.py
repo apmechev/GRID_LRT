5c822eddc9c825764d32b835ac0c4b19902ae274
Alexandar Mechev <apmechev@gmail.com>
2018-10-25 12:16:45 +0200

    adding mock of subprocess.Popen for travis authorization

--- GRID_LRT/tests/test_submit.py
+++ GRID_LRT/tests/test_submit.py
@@ -2,7 +2,7 @@ from GRID_LRT.application import submit
 import os 
 import glob
 import unittest
-
+import mock
 
 
 class JdlsubmitTest(unittest.TestCase):
@@ -12,30 +12,45 @@ class JdlsubmitTest(unittest.TestCase):
     def tearDown(self):
         pass
 
+    @mock.patch.object(subprocess, 'Popen', autospec=True)
     def test_make_tempfile(self):
+        mock_popen.return_value.returncode = 0
+        mock_popen.return_value.communicate.return_value = ("output", "Error")
         launcher = submit.JdlLauncher()
         file_path = launcher.make_temp_jdlfile()
         self.assertTrue(os.path.exists(file_path.name))
     
+    @mock.patch.object(subprocess, 'Popen', autospec=True)
     def test_launch_raises_error(self):
+        mock_popen.return_value.returncode = 0
+        mock_popen.return_value.communicate.return_value = ("output", "Error")
         launcher=submit.JdlLauncher()
         l = launcher.make_temp_jdlfile()
         with launcher:
             self.assertRaises(OSError, launcher.launch) #Will raise OSerror if glite-wms-job-submit doesn't exist
 
+    @mock.patch.object(subprocess, 'Popen', autospec=True)
     def test_default_args(self):
+        mock_popen.return_value.returncode = 0
+        mock_popen.return_value.communicate.return_value = ("output", "Error")
         launcher=submit.JdlLauncher()
         self.assertTrue(launcher.numjobs==1)
         self.assertTrue(launcher.token_type=='t_test')
         self.assertTrue(launcher.ncpu==1)
 
+    @mock.patch.object(subprocess, 'Popen', autospec=True)
     def test_Numjobs(self):
+        mock_popen.return_value.returncode = 0
+        mock_popen.return_value.communicate.return_value = ("output", "Error")
         launcher=submit.JdlLauncher(NCPU=10)
         self.assertTrue(launcher.ncpu==10)
         jdl_file=launcher.build_jdl_file()
         self.assertTrue("CPUNumber = 10"in jdl_file)
 
+    @mock.patch.object(subprocess, 'Popen', autospec=True)
     def test_LaunchFileExists(self):
+        mock_popen.return_value.returncode = 0
+        mock_popen.return_value.communicate.return_value = ("output", "Error")
         launcher=submit.JdlLauncher(NCPU=10)
         jdl_file=launcher.build_jdl_file()
         self.assertTrue(os.path.exists(launcher.launch_file))
