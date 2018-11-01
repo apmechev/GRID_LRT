4306b957f2c6d6aaca2cc150eeef00e5747e2287
Alexandar Mechev <apmechev@gmail.com>
2018-11-01 15:55:45 +0100

    moving mocked uberftp calls from str to bytes

--- GRID_LRT/tests/test_Storage.py
+++ GRID_LRT/tests/test_Storage.py
@@ -4,15 +4,15 @@ Need to scaffold test suite using mocked uberftp -ls results"""
 from  GRID_LRT.storage import gsifile
 import unittest
 
-output_sksp="""drwx------  1 lofsksp    lofsksp             512 Oct  8 10:44 archive
+output_sksp=bytes("""drwx------  1 lofsksp    lofsksp             512 Oct  8 10:44 archive
 drwx------  1 lofsksp    lofsksp             512 Oct 17 20:33 CI
 drwx------  1 lofsksp    lofsksp             512 May 10  2017 sksp_natalie
 drwx------  1 lofsksp    lofsksp             512 Jul 26 11:50 distrib
 drwx------  1 lofsksp    lofsksp             512 Sep 18 14:06 sandbox
 drwx------  1 lofsksp    lofsksp             512 Feb  5  2018 LGPPP
-drwx------  1 lofsksp    lofsksp             512 Jul 17 10:50 pipelines"""
+drwx------  1 lofsksp    lofsksp             512 Jul 17 10:50 pipelines""")
 
-output_sbx_test="""-r--------  1 lofsksp    lofsksp        80445440 Nov 17  2017 airflowtest1.tar
+output_sbx_test=bytes("""-r--------  1 lofsksp    lofsksp        80445440 Nov 17  2017 airflowtest1.tar
 -r--------  1 lofsksp    lofsksp       125491200 Nov 13  2017 test_data.tar
 -r--------  1 lofsksp    lofsksp        80435200 Oct  4  2017 josh_pref_cal2.tar
 -r--------  1 lofsksp    lofsksp           30720 Sep 24 15:03 calib_tutorial.tar
@@ -22,10 +22,9 @@ output_sbx_test="""-r--------  1 lofsksp    lofsksp        80445440 Nov 17  2017
 drwx------  1 lofsksp    lofsksp             512 Jan 23  2018 leah
 -r--------  1 lofsksp    lofsksp        80445440 Nov 23  2017 pref_cal1.tar
 -r--------  1 lofsksp    lofsksp        80445440 Nov 23  2017 pref_cal2.tar
--r--------  1 lofsksp    lofsksp           30720 Feb  2  2018 tutorial.tar"""
+-r--------  1 lofsksp    lofsksp           30720 Feb  2  2018 tutorial.tar""")
 
-output_one_file="""-r--------  1 lofsksp    lofsksp        80445440 Nov 17  2017 airflowtest1.tar"""
-
+output_one_file=bytes("""-r--------  1 lofsksp    lofsksp        80445440 Nov 17  2017 airflowtest1.tar""")
 
 
 from mock import MagicMock
