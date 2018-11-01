9c775fd408b30fc254d36fc9450b85318d085faa
Alexandar Mechev <apmechev@gmail.com>
2018-11-01 13:12:36 +0100

    removing cumbersome git hash/commit number

--- GRID_LRT/__init__.py
+++ GRID_LRT/__init__.py
@@ -26,37 +26,4 @@ __email__ = "LOFAR@apmechev.com"
 __status__ = "Production"
 __date__ = "2018-09-29"
 
-if sys.version_info[0:2] == (2,6): 
-    def check_output(*args, **kwargs):
-        process = Popen(stdout=PIPE, *args, **kwargs)
-        output, _ = process.communicate()
-        return output
 
-
-def format_version(version):
-    fmt = '{tag}.{commitcount}+{gitsha}'
-    parts = version.split('-')
-    assert len(parts) in (3, 4)
-    dirty = len(parts) == 4
-    tag, count, sha = parts[:3]
-    if count == '0' and not dirty:
-        return tag
-    return fmt.format(tag=tag, commitcount=count, gitsha=sha.lstrip('g'))
-
-def get_git_version():
-    repo = git.Repo(search_parent_directories=True)
-    tag  = str(repo.tags[-1])
-    return tag
-
-def get_git_hash():
-    git_hash = get_git_version()
-    git_hash = git_hash.split(__version__+".")[1]
-    return {'hash':git_hash.split('+')[1],
-            'number of commits after '+__version__:git_hash.split('+')[0]}
-
-#if socket.gethostname() != 'loui.grid.surfsara.nl':
-#    RuntimeWarning("You're not running on loui. Some features will not work!")
-
-if not call(["git", "branch"], stderr=STDOUT, stdout=open(os.devnull, 'w')) != 0:
-    __commit__ = get_git_hash()
-    __githash__ = __commit__['hash']
