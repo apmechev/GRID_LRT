0135b49e976d1b3ae9edc7138c2ef0bb922a6f73
Alexandar Mechev <apmechev@gmail.com>
2018-11-04 13:46:18 +0100

    test decode the year returned by uberftp

--- GRID_LRT/storage/gsifile.py
+++ GRID_LRT/storage/gsifile.py
@@ -103,7 +103,7 @@ class GSIFile(object):
 
     @staticmethod
     def _extract_date(data):
-        if str(data[-2]) not in ['2018','2017','2016','2015']:
+        if str(data[-2].decode('ascii')) not in ['2018','2017','2016','2015']:
             date = data[-4].decode('ascii')+" "+data[-3].decode('ascii')+" " + str(datetime.now().year)
             time = data[-2].decode('ascii')
         else:
