c863932789e2c29019c59e02f4b95cdf9fc5b67f
Alexandar Mechev <apmechev@gmail.com>
2019-02-13 11:01:05 +0100

    Update to 0.5.1 to include supported python versions

--- GRID_LRT/__init__.py
+++ GRID_LRT/__init__.py
@@ -12,11 +12,11 @@ __version__ = "0.6.0"
 __author__ = "Alexandar P. Mechev"
 __copyright__ = "2019 Alexandar P. Mechev"
 __credits__ = ["Alexandar P. Mechev", "Natalie Danezi", "J.B.R. Oonk"]
-__bibtex__ = """@misc{apmechev:2018,
+__bibtex__ = """@misc{apmechev:2019,
       author       = {Alexandar P. Mechev} 
-      title        = {apmechev/GRID_LRT: v0.5.0},
+      title        = {apmechev/GRID_LRT: v0.5.1},
       month        = sep,
-      year         = 2018,
+      year         = 2019,
       doi          = {10.5281/zenodo.1438833},
       url          = {https://doi.org/10.5281/zenodo.1438833}
     }"""
