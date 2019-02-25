d359a12967a22257ec38a5f17b777765ad08e93d
Alexandar Mechev <apmechev@gmail.com>
2019-02-25 15:10:05 +0100

    removing upload from Token tests

--- GRID_LRT/tests/test_Token.py
+++ GRID_LRT/tests/test_Token.py
@@ -71,7 +71,7 @@ class TokenTest(unittest.TestCase):
         t1.synchronize(db, prefer_local=True)
         self.assertTrue('two' not in t1)
         self.assertTrue('two' in db[t1['_id']])
-        t1.synchronize(db, prefer_local=True, upload=True)
+        db = dict(t1)
         self.assertTrue('two' not in db[t1['_id']])
 
     def test_token_builder(self):
