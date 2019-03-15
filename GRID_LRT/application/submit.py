ca79337b07b06456bb3d3121a8bf18c213a8e693
Alexandar Mechev <apmechev@gmail.com>
2019-03-15 14:59:54 +0100

    Updating submit module

--- GRID_LRT/application/submit.py
+++ GRID_LRT/application/submit.py
@@ -193,7 +193,7 @@ class JdlLauncher(object):
   DataAccessProtocol = {"gsiftp"};
   ShallowRetryCount = 0;
 
-  Requirements=(RegExp("%s",other.GlueCEUniqueID));
+  Requirements=(RegExp("gina.sara.nl:8443/cream-pbs-%s",other.GlueCEUniqueID));
   WholeNodes = %s ;
   SmpGranularity = %d;
   CPUNumber = %d;
