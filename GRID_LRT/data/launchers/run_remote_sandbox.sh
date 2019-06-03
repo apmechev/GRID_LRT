96d1d9d66bb94bb1dd8b3d913356778bc60c2fd5
Alexandar Mechev <apmechev@gmail.com>
2019-06-03 15:26:52 +0200

    Adding updated link to git repo

--- GRID_LRT/data/launchers/run_remote_sandbox.sh
+++ GRID_LRT/data/launchers/run_remote_sandbox.sh
@@ -19,11 +19,11 @@ set -x
 
 if type git &> /dev/null
 then
- git clone https://github.com/apmechev/GRID_picastools.git p_tools_git
+ git clone https://github.com/apmechev/GRID_PiCaS_Launcher.git p_tools_git
  cd p_tools_git || exit -100 
  cd ../ || exit -100
 else  #move this to testpy3
- wget -O master.zip https://github.com/apmechev/GRID_picastools/archive/master.zip
+ wget -O master.zip https://github.com/apmechev/GRID_PiCas_Launcher/archive/master.zip
  unzip master.zip -d p_tools_git/
 fi
 
